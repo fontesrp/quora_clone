ac6efa647cde385a23534f6d31784e139f55879b
Rodrigo Fontes <fontesr2@gmail.com>
2018-02-03 18:18:08 +0000

    Create Questions resource and add INSERT validations

---
+++ app/models/question.rb
@@ -0,0 +1,4 @@
+class Question < ApplicationRecord
+    belongs_to :user
+    validates :content, length: { maximum: 140 }, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :questions
+    validates :full_name, presence: true, length: { maximum: 256 }
+    validates :email, presence: true, length: { maximum: 256 }
+    validates :user_password, presence: true, length: { maximum: 256 }
+    validates :description, presence: true, length: { maximum: 1024 }
 end
